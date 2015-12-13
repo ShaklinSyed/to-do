00c1efe4d7e28f823324719128803e9b1b0d061a
Shaklin <shaklin_s@yahoo.com>
2015-12-13 21:24:54 +0530

    simplify

--- script.js
+++ script.js
@@ -1,74 +1,38 @@
 document.addEventListener("DOMContentLoaded",function(){
 	console.log("ready");
 
-	var listSize =0;
-
 	document.getElementById("btn").addEventListener("click",addItem);
-	var incompItemList = document.getElementById("incomp");
-	var inputTags = incompItemList.getElementsByTagName("input");
-
-	function moveNode(itemNo,whereTo){
-		var toMoveNode = document.getElementById("div"+itemNo);
-		if(whereTo == "comp"){
-			toMoveNode.setAttribute("class","compItem");
-			var compParent = document.getElementById("comp");
-		}
-		else if(whereTo == "incomp")
-		{
-			toMoveNode.setAttribute("class","inCompItem");
-			var compParent =document.getElementById("incomp");
-		}
-		compParent.appendChild(toMoveNode);
-	}
-
-//Moves node from one list to another
-	function move(){
-		for(var i=0;i<listSize;i++){
-			var item = document.getElementById(i);
-			var dive = document.getElementById("div"+i)
-			var diveClass = dive.getAttribute("class");
-
-			console.log(i,item, diveClass);
-			if(item.checked){
-				if(diveClass == "inCompItem"){
-					moveNode(i,"comp");
-					console.log("moved",i,"to comp");
-				}
-			}
-			else{
-				if(diveClass == "compItem"){
-					moveNode(i,"incomp");
-					console.log("moved",i,"to In comp");
-				}	
-			} 
-		}
-	}
+	var inCompList = document.getElementById("incomp");
+	var compList = document.getElementById("comp");
 
 // creates a node and appends incomp node
-
 	function addItem(){		
+		console.log("inside additem");
 		var textValue = document.getElementById("inp").value;
 		if(textValue !== ""){
-
-			var dive = document.createElement("div");
-			dive.setAttribute("class","inCompItem");
-			dive.setAttribute("id","div" + listSize);
-
-			var pe = document.createElement("p");
-			dive.appendChild(pe);
-			
-			var inputTag = document.createElement("input");
-			inputTag.setAttribute("type","checkbox");
-			inputTag.setAttribute("id",listSize)
-			inputTag.addEventListener("click",move);
-			pe.appendChild(inputTag);
-
-			var text = document.createTextNode(textValue);
-			pe.appendChild(text);	
-			
-			var incomp = document.getElementById("incomp");
-			incomp.appendChild(dive);
-			listSize++;
+			var div = document.createElement("div");
+			var text = document.createTextNode(textValue);	
+			div.appendChild(text);
+			addEvent(div);
+			inCompList.appendChild(div);
 		}
 	}
+
+//Adds event to move to compList
+	var addEvent = function(element){
+		element.addEventListener("click",function(){
+			this.removeEventListener("click");
+			compList.appendChild(this);
+			moveBack(this);
+		});
+	};
+
+// Adds event to move to inCompList
+	var moveBack = function(element){
+		element.addEventListener("click",function(){
+			this.removeEventListener("click");
+			inCompList.appendChild(this);
+			addEvent(this);
+		});
+	};
 });
